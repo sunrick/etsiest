7329409003362adc1ee942e8671c7de096df320b
Rickard Sunden <rickard.sunden@outlook.com>
2015-06-04 20:54:36 -0400

    Rough sketch of api data retrieval

--- lib/etsiest.rb
+++ lib/etsiest.rb
@@ -1,5 +1,42 @@
+require 'pry'
+require 'etsy'
+require 'sinatra'
 require "etsiest/version"
 
+Etsy.api_key = ENV['ETSY_KEY']
+
 module Etsiest
-  # Your code goes here...
+
+  class App < Sinatra::Base
+    enable :logging
+  
+    get '/etsy/search' do
+      Etsy.api_key = ENV['ETSY_KEY']
+      response = Etsy::Request.get('/listings/active', :includes => ['Images', 'Shop'], :keywords => 'whiskey')
+      a = JSON.parse(response.body)['results']
+      a.to_json
+    end
+
+    get '/' do
+    end
+
+
+    run! if app_file == $0
+  end
 end
+
+Etsy.api_key = ENV['ETSY_KEY']
+response = Etsy::Request.get('/listings/active', :includes => ['Images', 'Shop'], :keywords => 'whiskey') = JSON.parse(response.body)['results']
+response.map do |listing|
+  listing_hash = {
+    title: listing['title'],
+    price: listing['price'],
+    url: listing['url'],
+    shop: listing['Shop']['shop_name'],
+    image_url: listing['Images'][0]['url_fullxfull']
+  }
+  result << listing_hash
+  binding.pry
+end
+
+binding.pry
